6368a198830af60ac38f086b9c2039f654f3a49a
archanakushvaha <archanakushvaha735@gmail.com>
2026-07-28 13:56:03 +0530

    Add files via upload

---
+++ Pro_4_Functional_Treat.py
@@ -0,0 +1,123 @@
+data = []
+
+def add_data():
+
+    
+
+def data_summary():
+'''
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n*factorial(n - 1)
+
+def run_factorial():
+    print("\n====Calculate Factorial====")
+    num = int(input("Enter a number: "))
+    if num < 0:
+        print("Factorial is not defined for negative numbers.")
+    else:
+        result = factorial(num)
+        print(f"The factorial of {num} is: {result}")
+ '''       
+def filter_data():
+
+def sort_data():
+
+def display_statistics():
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+# Main Menu
+
+while True:
+    print("\n=====Welcome to Data Analyzer Program=====")
+    print("1. Input Data")
+    print("2. Display Data Summary")
+    print("3. Calculate Factorial")
+    print("4. Fliter Data by Threshold")
+    print("5. Sort Data")
+    print("6. Display Dataset Statistics")
+    print("7. Exit Program")
+
+    choice = input("Please enter your choice (1-7): ")
+
+    if choice == "1":
+        add_data()
+    elif choice == "2":
+        data_summary()
+    elif choice == "3":
+        run_factorial()
+    elif choice == "4":
+        filter_data()
+    elif choice == "5":
+        sort_data()
+    elif choice == "6":
+        display_ststistics()
+    elif choice == "7":
+        print("\n Thank you for usingthe program. Goodbye!")
+        break
+    else:
+        print("\n Invalid choice! Please try again.")
+'''        
+
+        
