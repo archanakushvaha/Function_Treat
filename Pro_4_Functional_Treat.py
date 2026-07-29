af19bede72164020f5cda0bca81580a6089df15a
archanakushvaha <archanakushvaha735@gmail.com>
2026-07-29 21:35:14 +0530

    Update Pro_4_Functional_Treat.py

--- Pro_4_Functional_Treat.py
+++ Pro_4_Functional_Treat.py
@@ -1,50 +1,68 @@
-data = []
+print("Welcome to the Data Analyzer and Transformer Program")
+Arrays=[]
 
 def add_data():
+    global Arrays
+    
+    print("1.1D array")
+    print("2.2D array")
 
-    global data
+    option = int(input("Enter a number for array:"))
 
-    data = list(map(int ,input("Enter data for a 1D array(separated by spaces): ").split()))
+    if option == 1:
 
-    print("Data has been stored successfully!")
+        num =list(map(int , input("Enter data for a 1D array(seprated by space):").split()))
 
-def data_summary():
-    if len(data) == 0:
-        print("\n No Data Found!!! ")
-        return
+        Arrays.append(num)
+        print("Data has been stored successfully!")
 
-    print("\n Data Summary:")
-    print("Total elements: " ,len(data) )
-    print("Minimum value: ",min(data))
-    print("Maximum value: ",max(data))
-    print("Sum of all value: ",sum(data))
+    elif option == 2:
 
-    average = sum(data)/len(data)
-    print("Average value :" , average)
+        num = []
+
+        for i in range(3):
+            row = list(map(int , input(f"Enter Row {i + 1} : ").split()))
+
+            num.append(row)
+
+            
+        Arrays.append(num)
+        print("Data has been stored successfully!")
+ 
+    else:
+        print("Invalid choice")
+
+
+def Display_data():
+  
+    print("\nData Summary:")
+    print("- Total elements:",len(Arrays[0]))
+    print("- Minimum Value:",min(Arrays[0]))
+    print("- Maximum Value:",max(Arrays[0]))
+    print("- Sum of all Values:",sum(Arrays[0]))
+    print("- avarage value:",sum(Arrays[0])/len(Arrays[0]))
     
 def calculate_factorial():
+    
     n = int(input("Enter a number to calulate its factorial: "))
     def factorial(n):
-        if n == 1:
+        if n == 0 or n == 1:
             return 1
         return n*factorial(n-1)
-    print(factorial(n))
-    
-       
+    print("Factorial is : ", factorial(n))
+
 def filter_data():
 
-    if len(data) == 0:
-        print("\n No Data Found!!! ")
-        return
-    threshold = int(input("Enter a threshold value to filter out data above this value: "))
+    value=int(input("Enter a threshold value to filter out data above this value:"))
 
-    my_filter = lambda x: x >= threshold
-    result = list(filter(my_filter , data))
-
-    print(f"Filtered Data (value >= {threshold}):  {result}")
+    numbers=list(filter(lambda x:x >= value,Arrays[0]))
+   
+    print(filter_data.__doc__)
+    print(numbers)
+    return numbers
 
 def sort_data():
-    if len(data) == 0:
+    if len(Arrays) == 0:
         print("\n No Data Found!")
         return
 
@@ -55,59 +73,60 @@ def sort_data():
     choice = input("Enter your choice :")
 
     if choice == "1":
-        print("Sorted Data in Ascending order :", sorted(data))
+        print("Sorted Data in Ascending order :", sorted(Arrays))
     else:
-        print("Sorted Data in Descending order :", sorted(data , reverse=True))
+        print("Sorted Data in Descending order :", sorted(Arrays , reverse=True))
     
-def display_statistics():
+def display_dataset(*args,**kwargs):
 
-    if len(data) == 0:
-        print("\n No data found!!!")
-        return
+    Minimum=min(args)
+    Maximum=max(args)
+    Total=sum(args)
+    Average=sum(args)/len(args)
     
-    minimum = min(data)
-    maximum = max(data)
-    total = sum(data)
-    average = total / count
+    if kwargs.get("show",True):
+
+        print("- Data Statistics:") 
+        print("- Minimum value:",Minimum)
+        print("- Maximum value:",Maximum)
+        print("- Sum of all values:",Total)
+        print("- Average value:",Average)
+
+    return Minimum,Maximum,Total,Average
     
-    print("Dataset Statistics:")
-    print(f"-Minimum value : ",minimum)
-    print(f"-Maximum value : ",maximum)
-    print(f"-Sum of Element : ",total)
-    print(f"-Average value : ",average)
-    
-    
-# Main Menu
+def exit():
+    print("Thank you for using the data Analyzer and Transformer")
+    print("Program. Goodbye!")
 
 while True:
-    print("\n Welcome to Data Analyzer and Transformer Program")
-    print("Main Menu: ")
+
+    print("\nMain Menu:")
     print("1. Input Data")
-    print("2. Display Data Summary(Built-in Functions)")
-    print("3. Calculate Factorial(Recursion)")
-    print("4. Fliter Data by Threshold(Lambda function)")
-    print("5. Sort Data")
-    print("6. Display Dataset Statistics(Return Multiple values)")
-    print("7. Exit Program")
+    print("2. Display Data Summery(Built-in Functions)")
+    print("3. Calculate Factorial(Recurtion)")
+    print("4. Filter Data by Threshld(Lamda Function)")
+    print("5.Sort Data")
+    print("6.Display Dataset Statistics(Return Multiple Values)")
+    print("7.Exit Program")
 
-    choice = input("Please enter your choice: ")
+    choice = int(input("Please enter your choice:"))
 
-    if choice == "1":
+    if choice==1:
         add_data()
-    elif choice == "2":
-        data_summary()
-    elif choice == "3":
+    elif choice==2:
+        Display_data()
+    elif choice==3:
         calculate_factorial()
-    elif choice == "4":
+    elif choice==4:
         filter_data()
-    elif choice == "5":
+    elif choice==5:
         sort_data()
-    elif choice == "6":
-        display_statistics()
-    elif choice == "7":
-        print("\n Thank you for using the Data Analyzerand Transformer program. Goodbye!")
+    elif choice==6:
+        Minimum,Maximum,Sum,Avarage=display_dataset(*Arrays[0],show=True)
+    elif choice==7:
+        exit()
         break
     else:
-        print("\n Invalid choice! Please try again.")        
+        print("\n Invalid data")
 
-        
+    
